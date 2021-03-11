3c55a06f81828766fec1b796a0d08178cfd6ad3c
Younessy <oubelkacemy@gmail.com>
2021-03-11 20:16:10 +0100

    Form update

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,22 @@
 const navToggle = document.getElementById("navToggle"); 
 const navlinks = document.getElementById("navlinks");
 const form = document.getElementById("form");
-const formToggle = document.getElementById("showingform");
+const showingForm = document.getElementById("showingform");
+const hideForm = document.getElementById("hideform");
 
 navToggle.addEventListener("click",function (){
   navlinks.classList.toggle("active");
 });
 
-formToggle.addEventListener("click",function (){
+showingForm.addEventListener("click",function (){
+  hideForm.style.display = "block";
+  showingForm.style.display = "none";
+  form.style.display = "block";
   
-  
-  form.classList.toggle("displayform");
+});
+
+hideForm.addEventListener("click",function(){
+  hideForm.style.display="none";
+  showingForm.style.display="block";
+  form.style.display="none";
 });
