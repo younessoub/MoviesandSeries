ea054ec63809f4cba271084150f04cf43ab8d7ef
Younessy <oubelkacemy@gmail.com>
2021-03-11 21:23:03 +0100

    Form

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,12 @@ const navlinks = document.getElementById("navlinks");
 const form = document.getElementById("form");
 const showingForm = document.getElementById("showingform");
 const hideForm = document.getElementById("hideform");
+const type = document.getElementById('type');
+const name = document.getElementById('name');
+const rating = document.getElementById('Rating');
+const submit = document.getElementById('Add');
+
+
 
 navToggle.addEventListener("click",function (){
   navlinks.classList.toggle("active");
@@ -20,3 +26,51 @@ hideForm.addEventListener("click",function(){
   showingForm.style.display="block";
   form.style.display="none";
 });
+
+submit.addEventListener('click',function (e){
+  e.preventDefault();
+  var Type = type.value;
+  var Name = name.value;
+  var Rating = rating.value;
+  name.value='';
+  
+  if(Type==='M'){
+    var m = new movie(Name,Rating);
+    movies.push(m);
+  }
+  if(Type==='T'){
+    var t = new tvshow(Name,Rating);
+    tvshows.push(t);
+  }
+  
+  
+  for(let i=0;i<movies.length;i++){
+    console.log(movies[i]);
+  } 
+  for(let i=0;i<tvshows.length;i++) {
+    console.log(tvshows[i]);
+  } 
+  /*console.log ;
+   console.log (Name);
+    console.log (Rating);*/
+});
+
+function sort(t,n,r){
+  
+}
+
+function movie(n,r){
+  
+  this.type = "M";
+  this.name = n;
+  this.rating = r;
+}
+
+function tvshow(n,r){
+  this.type = "T";
+  this.name = n;
+  this.rating = r;
+}
+
+var movies = [];
+var tvshows = [];
