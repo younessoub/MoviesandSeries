2c75228055a18dd3cc54391238150b53b3b3f808
Younessy <oubelkacemy@gmail.com>
2021-03-11 01:06:55 +0100

    Added form

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
 const navToggle = document.getElementById("navToggle"); 
 const navlinks = document.getElementById("navlinks");
+const form = document.getElementById("form");
+const formToggle = document.getElementById("showingform");
 
 navToggle.addEventListener("click",function (){
   navlinks.classList.toggle("active");
+});
+
+formToggle.addEventListener("click",function (){
+  
 });
